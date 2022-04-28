ec7aaa97b685a93320fd57f878201a90b1ed715a
raylok.finnet <ravi.leal@finnet.com.br>
2022-04-28 13:50:57 -0300

    feat: inject repository on post tax id service

--- src/TaxId/Index.ts
+++ src/TaxId/Index.ts
@@ -1,9 +1,12 @@
 import PostTaxIdController from './Actions/PostTaxIdController';
 import PostTaxIdService from './Services/PostTaxIdService';
+import TaxIdRepository from './Storage/TaxIdRepository';
 import PostTaxIdTransformer from './Transformers/PostTaxIdTransformer';
+
+const taxIdRepository = new TaxIdRepository();
 
 const postTaxIdTransformer = new PostTaxIdTransformer();
 
-const postTaxIdService = new PostTaxIdService(postTaxIdTransformer);
+const postTaxIdService = new PostTaxIdService(postTaxIdTransformer, taxIdRepository);
 
 export const postTaxIdController = new PostTaxIdController(postTaxIdTransformer, postTaxIdService);
