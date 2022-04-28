e64985c51fb84dd9ea0f8e79bf5e9eacc3ee51c6
raylok.finnet <ravi.leal@finnet.com.br>
2022-04-28 13:20:34 -0300

    feat: add public route for taxId posting

--- src/Routes/PublicRoutes.ts
+++ src/Routes/PublicRoutes.ts
@@ -1,10 +1,15 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { isAliveController } from '../Home/Index';
+import { postTaxIdController } from '../TaxId/Index';
 
 const publicRoutes = express.Router();
 
 publicRoutes.get('/', (request: Request, response: Response, nextFunction: NextFunction) => {
     return isAliveController.handle(request, response, nextFunction);
+});
+
+publicRoutes.post('/taxId', (request: Request, response: Response, nextFunction: NextFunction) => {
+    return postTaxIdController.handle(request, response, nextFunction);
 });
 
 export { publicRoutes };
