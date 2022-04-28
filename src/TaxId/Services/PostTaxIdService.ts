a8a6c98a8b97525034dc414b5ad07d7bdc3f18cc
raylok.finnet <ravi.leal@finnet.com.br>
2022-04-28 13:50:34 -0300

    feat: use tax id repository for database operations

--- src/TaxId/Services/PostTaxIdService.ts
+++ src/TaxId/Services/PostTaxIdService.ts
@@ -1,22 +1,20 @@
 import IDatabaseTransformer from '../../Api/Transformers/IDatabaseTransformer';
 import TaxIdDto from '../Dtos/TaxIdDto';
 import TaxIdEntity from '../Storage/TaxIdEntity';
-import { KnexInstance } from '../../Database/knex';
 import ResourceAlreadyExistsException from '../../Api/Exception/ResourceAlreadyExistsException';
+import ITaxIdRepository from '../Storage/ITaxIdRepository';
 
 export default class PostTaxIdService {
-    constructor(private transformer: IDatabaseTransformer<TaxIdDto, TaxIdEntity>) {}
+    constructor(private transformer: IDatabaseTransformer<TaxIdDto, TaxIdEntity>, private readonly repository: ITaxIdRepository) {}
 
     public async invoke(dto: TaxIdDto) {
         const entity = <TaxIdEntity>await this.transformer.toEntity(dto);
 
-        try {
-            await KnexInstance.insert(entity).into('taxIds');
-        } catch (error) {
-            if (error.detail.includes('already exists')) {
-                throw new ResourceAlreadyExistsException([`TaxId '${dto.taxId}' already exists.`]);
-            }
-            throw error;
+        const taxIdEntity = await this.repository.findByTaxId(dto.taxId);
+        if (taxIdEntity && taxIdEntity.taxId) {
+            throw new ResourceAlreadyExistsException([`TaxId '${dto.taxId}' already exists.`]);
         }
+
+        await this.repository.save(entity);
     }
 }
