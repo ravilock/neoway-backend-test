29ea7bdb3355859b4b203b5a92a105b7c0cba95f
raylok.finnet <ravi.leal@finnet.com.br>
2022-04-28 13:17:22 -0300

    feat: create tax id table migration

---
+++ migrations/20220428115406_createTableTaxIds.ts
@@ -0,0 +1,20 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('taxIds', function (table) {
+        table.primary(['id']);
+        table.increments('id');
+        table.string('uuid').notNullable().unique();
+        table.string('taxId').notNullable().unique();
+        table.string('accountName').notNullable();
+        table.date('startDate').notNullable();
+        table.date('createdAt').notNullable().defaultTo(knex.fn.now());
+        table.date('updatedAt').notNullable().defaultTo(knex.fn.now());
+        table.date('deleteAt').nullable().defaultTo(null);
+        table.boolean('deleted').nullable().defaultTo(false);
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('taxIds');
+}
