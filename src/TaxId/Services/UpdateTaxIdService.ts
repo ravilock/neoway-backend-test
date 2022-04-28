64c6e3bfedf5f0eef1bb430eb1a769e93c184176
raylok.finnet <ravi.leal@finnet.com.br>
2022-04-28 17:58:14 -0300

    feat: add update tax id service

---
+++ src/TaxId/Services/UpdateTaxIdService.ts
@@ -0,0 +1,22 @@
+import IDatabaseTransformer from '../../Api/Transformers/IDatabaseTransformer';
+import TaxIdDto from '../Dtos/TaxIdDto';
+import TaxIdEntity from '../Storage/TaxIdEntity';
+import ITaxIdRepository from '../Storage/ITaxIdRepository';
+import ResourceNotFoundException from '../../Api/Exception/ResourceNotFoundException';
+
+export default class UpdateTaxIdService {
+    constructor(private transformer: IDatabaseTransformer<TaxIdDto, TaxIdEntity>, private readonly repository: ITaxIdRepository) {}
+
+    public async invoke(dto: TaxIdDto) {
+        const entity = <TaxIdEntity>await this.transformer.toEntity(dto);
+
+        const taxIdEntity = await this.repository.findByUuid(entity.uuid);
+        if (!taxIdEntity || !taxIdEntity.uuid) {
+            throw new ResourceNotFoundException();
+        }
+
+        console.log(entity);
+
+        await this.repository.update(entity);
+    }
+}
